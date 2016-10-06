1828d9f6a2bdbd7318d8b2634ae5627af3162b4b
TAE YOUNG (Daniel) KIM <ty2kim@gmail.com>
2016-10-06 16:21:53 +0000

    first commit of sort2.js

---
+++ sort2.js
@@ -0,0 +1,20 @@
+// var array = [10, 2, 5, 1, 9];
+//
+// array.sort(function(a,b){
+//   console.log(`a: ${a}, b: ${b} -> ${a-b}`);
+//   return a - b;
+// });
+//
+// console.log(array);
+var students = [
+  { id: 1, name: "bruce",    age: 40 },
+  { id: 2, name: "zoidberg", age: 22 },
+  { id: 3, name: "alex",     age: 22 },
+  { id: 4, name: "alex",     age: 30 }
+];
+
+students.sort(function(a, b){
+  return b.age - a.age;
+});
+
+console.log(students);
